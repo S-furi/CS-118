73a08a426bd9337556859e33947cc29702f617c6
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-13 16:03:28 +0200

    basics utilities for package handling

---
+++ src/utilities.py
@@ -0,0 +1,33 @@
+import sys
+import hashlib
+import json
+
+"""
+Header Composition:
+    -size
+    -operation (LIST, GET, PUT, FIN)
+    -checksum
+    -payload (in case of FIN, here is stored the success of the operation)
+"""
+
+def build_packet(operation : str, payload) -> dict:
+    pkt = {
+        "size" : sys.getsizeof(payload),
+        "op" : operation,
+        "checksum" : None,
+        "payload" : payload
+    }
+    return pkt
+
+def compute_checksum(pkt) -> str:
+    return hashlib.sha1(json.dumps(pkt).encode()).hexdigest()
+
+def decode_package(raw_data) -> dict:
+    return dict(json.loads(raw_data))
+
+def checksum_integrity(pkt : dict) -> bool:
+    original = pkt.get("checksum")
+    pkt["checksum"] = None
+    computed = compute_checksum(pkt)
+    return computed == original
+    
