ee464cbe5d3c8f835f4aa09d4627e416d594bd62
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-15 00:36:01 +0200

    adapded function choice with PUT

--- src/client/client_runner.py
+++ src/client/client_runner.py
@@ -6,17 +6,23 @@ while True:
     op = input("Please, select one of the following operations\n\
                 1) Listing of all the files contained in the server\n\
                 2) Get a file contained in the server\n\
-                3) EXIT\n")
+                3) Upload a file to the server\n\
+                4) EXIT\n")
     
     if op == '1':
         clnt.get_list()
     elif op == '2':
-        filename = input("Insersci il nome del file: ")
+        filename = input("Insert file name: ")
         filename = filename.lower()
         if not clnt.get_file(filename):
             print("Client failed")
             sys.exit(0)
     elif op == '3':
+        filename = input("Insert file name: ")
+        filename = filename.lower()
+        clnt.upload_file(filename)
+    
+    elif op == '4':
         print("Bye bye")
         sys.exit(0)
 
