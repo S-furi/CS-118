251092dfc582e65f2787ada7cb7be316e6a57725
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-15 00:33:43 +0200

    first draft of PUT function, need to be cleaned and checks controls and return status

--- src/server/request_manager.py
+++ src/server/request_manager.py
@@ -1,11 +1,14 @@
 import base64
+import pickle
 import logging
 import math
 from threading import Thread
 import socket as sock
 from utilities import *
 import os
+from pathlib import Path
 import signal
+import shutil
 
 class RequestManager(Thread):
     
@@ -19,6 +22,7 @@ class RequestManager(Thread):
         self.file_dir = "./files"
         self.finish_sending = False
         self.sending_buffer_size = 2048
+        self.receiving_buffer_size = 4096
         signal.signal(signal.SIGINT, self._close)
     
     def run(self) -> None:
@@ -31,7 +35,11 @@ class RequestManager(Thread):
                     self._send_packet("FIN", "ok")
             else:
                 self._send_packet("FIN", "failure")
+        if self.request_op == "PUT":
+            self._upload_file(self.request_payload, self.pakcet_to_deliver)
+
         self._close()
+        
 
     def _listing(self) -> bool:
         try:
@@ -65,7 +73,54 @@ class RequestManager(Thread):
         except Exception as e:
             logging.warning("An error occurred: " + e)
             return False
+
+    def _upload_file(self, payload : str, seqno : str) -> bool:
+        if seqno == -1:
+            if  os.path.isdir("./tmp"):
+                shutil.rmtree("./tmp")
+            os.mkdir("./tmp")
+            filepath = "./tmp/" + payload
+            Path(filepath).touch()
+            #Server ready to receive
+            self._send_packet("PUT", None)
+        else:
+            filename = os.listdir("./tmp")[0]
+            filename = filename.replace("\'","")
+            dump_path = "./tmp/dump.txt"
+
+            chunck = {
+                "seqno" : seqno,
+                "payload" : payload
+            }
+            w = open(dump_path, "a")
+            w.write(json.dumps(chunck))
+            w.close()
             
+            if sys.getsizeof(base64.b64decode(payload.encode())) < self.sending_buffer_size:
+                #last packet
+                self._compose_file(dump_path, f"./tmp/{filename}")
+            
+
+    def _compose_file(self, dump : str, path : str):
+        f = open(dump, 'r')
+        chuncks = f.read().replace("}{", "};{")
+
+        lst = []
+        for c in chuncks.split(";"):
+            lst.append(json.loads(c))
+        lst = sorted(lst, key=lambda t : t['seqno'])
+
+        w = open(path, 'wb')
+        for obj in lst:
+            load = base64.b64decode(obj.get("payload"))
+            w.write(load)
+        self._save_file(path)
+
+    def _save_file(self, filepath):
+        shutil.move(filepath, self.file_dir)
+        shutil.rmtree("./tmp")
+
+
     def _send_block(self, filename : str, seqno: int):
         path = self.file_dir + "/" + filename
         if seqno >= math.floor(os.path.getsize(path) / self.sending_buffer_size):
