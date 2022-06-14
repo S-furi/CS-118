80f0a93991c8f0dc76936e08d3f0a9f2e3a1f0f4
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-14 12:41:40 +0200

    Client now waits and requests cunchks of the packets, instead of passively receive a wave of packets without control

--- src/client/client.py
+++ src/client/client.py
@@ -3,15 +3,17 @@ import socket as sock
 import os
 import sys
 import logging
+from time import sleep
 sys.path.insert(0, "..")
 from utilities import *
 
 class Client:
     def __init__(self, server_addr):
         self.socket = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
-        self.socket.settimeout(4)
+        self.socket.settimeout(3)
         self.server_addr = server_addr
         self.buffer_size = 4096
+        self.sending_rate = 2048
         self.files_dir = "./downloads"
         if not os.path.isdir(self.files_dir):
             os.mkdir(self.files_dir)
@@ -20,51 +22,67 @@ class Client:
         self._send_pkt("LIST", None)
         data, addr = self.socket.recvfrom(self.buffer_size)
         if self._check_incoming_package(data):
+            pkg = decode_package(data)
             print("Files available for download:")
-            for name in data.get('payload').split():
+            for name in pkg.get('payload').split():
                 print(f'-{name}')
 
     def get_file(self, filename : str):
-        self._send_pkt("GET", filename)
-        data, addr = self.socket.recvfrom(self.buffer_size)
-        #First message that server sends is how many packets it will send to the client
-        data = self._check_incoming_package(data)
-        if data:
-            expected_packages = data.get('payload')
-            file = self.files_dir + "/" +"copy_"+ filename
-            file_to_write = open(file, 'wb')
-            bytes_count = 0
-            for i in range(int(expected_packages)):
-                data, addr = self.socket.recvfrom(self.buffer_size)
-                bytes_count = sys.getsizeof(data)
-                data = self._check_incoming_package(data)
-                payload = data.get('payload')
-                data_chunk = base64.b64decode(payload.encode())
-                file_to_write.write(data_chunk)
-            print(f"received {i} packages and {bytes_count} bytes in total")
-            data, addr = self.socket.recvfrom(self.buffer_size)
-            data = decode_package(data)
-            if i+1 == expected_packages and (data.get('op') == "FIN" and data.get('payload') == "ok") :
-                print(f"\n*** SUCCESFULLY DOWNLOADED \'{filename}\' ***\n")
-            else:
-                print("something went wrong during download, try again")
-            
+        try:
+            print(f"Sending request for {filename} to the server")
+            self._send_pkt("GET", filename, -1)
+            raw_data, addr = self.socket.recvfrom(self.buffer_size)
+            sleep(1)    
+            data = decode_package(raw_data)
+            if self._check_incoming_package(raw_data) and not (data.get('op') == "FIN"):
+                
+                expected_packets = data.get('payload')
+                
+                filepath = self.files_dir + "/copy_" + filename
+                
+                f = open(filepath, 'w+b')
+                for i in range(expected_packets):
+                    self._send_pkt("GET", filename, i)
+                    raw_data, addr = self.socket.recvfrom(self.buffer_size)
+                    pkg = decode_package(raw_data)
+                    rcv_seqno = pkg.get('seqno')
+                    
+                    if rcv_seqno != i:
+                        logging.error(f'Expected #{i} but received #{rcv_seqno}, aborting operation try again')
+                        return False
+                    
+                    chunck = base64.b64decode(pkg.get('payload').encode())
+                    f.write(chunck)
 
-    def _check_incoming_package(self, raw_data : bytes) -> dict:
-        data = decode_package(raw_data)
-        if data.get('op') == "FIN" and data.get('payload') == "failure":
+                f.close()
+            else:
+                logging.warning("The file is not found")
+        except sock.timeout:
+            logging.error("TIME OUT!")
+            return False
+        finally:
+            data, addr = self.socket.recvfrom(self.buffer_size)
+            if not self._check_incoming_package(data):
+                logging.error("Something went wrong during download, please try again")
+            else:
+                print(f"{filename} succesfully downloaded")
+            self.socket.close()
+        return True
+
+    def _check_incoming_package(self, raw_data : bytes) -> bool:
+        pkt = decode_package(raw_data)
+        if pkt.get('op') == "FIN" and pkt.get('payload') == "failure":
             print("PACKAGE FAILED")
-            return None
-        if checksum_integrity(data):
-            return data
+            return False
+        if checksum_integrity(pkt):
+            return True
         else:
             logging.error("checksum is incorrect: package may be corrupted...")
-            return None
+            return False
 
-    def _send_pkt(self, operation : str, payload):
-        pkt = build_packet(operation, payload)
+    def _send_pkt(self, operation : str, payload, seqno=0):
+        pkt = build_packet(operation, payload, seqno)
         pkt["checksum"] = compute_checksum(pkt)
         data = json.dumps(pkt)
         self.socket.sendto(data.encode(), self.server_addr)
-        print(f"{operation} sento to server")
         
