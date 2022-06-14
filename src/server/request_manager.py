ce7a008e1ac70e4e3881eb3a7c1b28b2fb5c76ef
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-14 10:38:36 +0200

    catching socket exceptions

--- src/server/request_manager.py
+++ src/server/request_manager.py
@@ -1,4 +1,5 @@
 import base64
+from stat import FILE_ATTRIBUTE_NORMAL
 from threading import Thread
 import socket as sock
 import sys
@@ -12,6 +13,7 @@ class RequestManager(Thread):
     def __init__(self, addr, operation : str, payload=None) -> None:
         super().__init__()
         self.socket = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
+        self.socket.settimeout(2)
         self.cl_addr = addr
         self.request_op = operation
         self.request_payload = payload
@@ -35,30 +37,38 @@ class RequestManager(Thread):
         
 
     def _get_file(self, filename : str):
-        if os.listdir(self.file_dir).__contains__(filename):
-            print(f"File present, dividing it into chuncks of {2048} bytes")
-            filename = self.file_dir + "/" + filename
-            splitFile = list(self._divide_chuncks(filename, self.sending_buffer_size))
-            num_packet_to_send = len(splitFile)
-            print(f'{filename} will be divided into {num_packet_to_send} packets')
-            self._send_packet("GET", num_packet_to_send)
-            time.sleep(3)
+        try:
+            if os.listdir(self.file_dir).__contains__(filename):
+                print(f"{filename} is present")
+                path = self.file_dir + "/" + filename
+                splitFile = list(self._divide_chuncks(path, self.sending_buffer_size))
+                num_packet_to_send = len(splitFile)
+                print(f'{filename} is {os.path.getsize(path)} bytes and will be divided into {num_packet_to_send} packets')
+                self._send_packet("GET", num_packet_to_send)
+                time.sleep(2)
 
-            for content in splitFile:
-                payload = base64.b64encode(content).decode('utf-8')
-                self._send_packet("GET", payload)
-            print(f"Finished sending {filename}")
-        else:
-            print(f"\"{filename}\" not found!")
+                for content in splitFile:
+                    payload = base64.b64encode(content).decode('utf-8')
+                    self._send_packet("GET", payload)
+                print(f"Finished sending {filename}")
+                time.sleep(1)
+                self._send_packet("FIN", "ok")
+            else:
+                print(f"\"{filename}\" not found!")
+                self._send_packet("FIN", "failure")
+        except Exception as e:
+            print("File transfer failed: " + e)
             self._send_packet("FIN", "failure")
+        finally:
+            self._close()
 
     def _divide_chuncks(self, filename : str, size : int): 
         with open(filename, 'rb') as f:
             while content := f.read(size):
                 yield content
 
-    def _send_packet(self, operation : str, paylaod : str):
-        pkt = build_packet(self.request_op, paylaod)
+    def _send_packet(self, operation : str, paylaod : str, seqno=0):
+        pkt = build_packet(operation, paylaod, seqno)
         pkt["checksum"] = compute_checksum(pkt)
         self.socket.sendto(json.dumps(pkt).encode(), self.cl_addr)
     
