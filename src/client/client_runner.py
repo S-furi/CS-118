4a3b2e58d6ddb31ea936633bb515d80c0ccb3a9a
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-14 12:40:10 +0200

    interactive serviceces choosing

--- src/client/client_runner.py
+++ src/client/client_runner.py
@@ -1,4 +1,24 @@
+import sys
 from client import Client
 
-clnt = Client(('localhost', 8080))
-clnt.get_file("Beemovie.txt")
+while True:
+    clnt = Client(('localhost', 8080))
+    op = input("Please, select one of the following operations\n\
+                1) Listing of all the files contained in the server\n\
+                2) Get a file contained in the server\n\
+                3) EXIT\n")
+    
+    if op == '1':
+        clnt.get_list()
+    elif op == '2':
+        filename = input("Insersci il nome del file: ")
+        filename = filename.lower()
+        clnt.get_file(filename)
+    elif op == '3':
+        print("Bye bye")
+        sys.exit(0)
+
+    cont = input("Do you want to continue?[Y,n]: ")
+    cont = cont.lower()
+    if not(cont == 'y' or cont == ""):
+        sys.exit(0)
