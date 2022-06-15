29d1c8b539d9e9a31e8477cb7281f73f2c944647
Stefano Furi <stefano.furi@studio.unibo.it>
2022-06-15 14:00:33 +0200

    Now outputs operation outcomes

--- src/client/client_runner.py
+++ src/client/client_runner.py
@@ -1,32 +1,39 @@
 import sys
 from client import Client
 
-while True:
-    clnt = Client(('localhost', 8080))
-    op = input("Please, select one of the following operations\n\
-                1) Listing of all the files contained in the server\n\
-                2) Get a file contained in the server\n\
-                3) Upload a file to the server\n\
-                4) EXIT\n")
-    
-    if op == '1':
-        clnt.get_list()
-    elif op == '2':
-        filename = input("Insert file name: ")
-        filename = filename.lower()
-        if not clnt.get_file(filename):
-            print("Client failed")
-            sys.exit(0)
-    elif op == '3':
-        filename = input("Insert file name: ")
-        filename = filename.lower()
-        clnt.upload_file(filename)
-    
-    elif op == '4':
-        print("Bye bye")
-        sys.exit(0)
+def display_outcome(outcome : bool, op_name : str):
+    msg = "succeed" if outcome else "failed"
+    print(f"{op_name} {msg}")
 
-    cont = input("Do you want to continue?[Y,n]: ")
-    cont = cont.lower()
-    if not(cont == 'y' or cont == ""):
-        sys.exit(0)
+def main():
+    while True:
+        clnt = Client(('localhost', 8080))
+        op = input("Please, select one of the following operations\n\
+                    1) Listing of all the files contained in the server\n\
+                    2) Get a file contained in the server\n\
+                    3) Upload a file to the server\n\
+                    4) EXIT\n")
+        
+        if op == '1':
+            display_outcome(clnt.get_list(), "LIST")
+        elif op == '2':
+            filename = input("Insert file name: ")
+            filename = filename.lower()
+            display_outcome(clnt.get_file(filename), "GET")
+        elif op == '3':
+            filename = input("Insert file name: ")
+            filename = filename.lower()
+            display_outcome(clnt.upload_file(filename), "PUT")
+        
+        elif op == '4':
+            print("Bye bye")
+            sys.exit(0)
+
+        cont = input("Do you want to continue?[Y,n]: ")
+        cont = cont.lower()
+        if not(cont == 'y' or cont == ""):
+            clnt.close()
+            sys.exit(0)
+
+if __name__ == "__main__":
+    main()
